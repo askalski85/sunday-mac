0232010692bd6a265bebe9d9073873d0731d9340
Arkadiusz Skalski <askalski85@gmail.com>
2018-01-26 10:18:29 +0100

    Added google disk sync and iterm2

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+mkdir -p $HOME/.sunday-mac/bin
+grep -r sunday-mac $HOME/.bash_profile || echo 'export PATH=$PATH:$HOME/.sunday-mac/bin' >> $HOME/.bash_profile
+rsync -av --delete $PWD/bin/ $HOME/.sunday-mac/bin
+
 bash apps/homebrew.sh
 bash apps/atom.sh
 bash apps/docker.sh
+bash apps/iterm2.sh
+bash apps/google.sh
